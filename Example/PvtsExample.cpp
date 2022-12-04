21b2365103e73d5e0ed0b613cca3795f2a571548
Masayuki Suzuki <m-suzuki@advancesoft.jp>
2022-12-04 23:26:45 +0900

    Add a missing file

---
+++ Example/PvtsExample.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+#include "kvs/KVSMLStructuredVolumeObject"
+#include "kvs/StructuredVolumeExporter"
+#include "kvs/StructuredVolumeObject"
+
+#include "CvtMode.h"
+#include "Exporter/VtkExporter.h"
+#include "FileFormat/VTKXMLPStructuredGrid/VtkXmlPStructuredGrid.h"
+#include "FileFormat/VTKXMLStructuredGrid/VtkXmlStructuredGrid.h"
+#include "Importer/VtkImporter.h"
+
+void UniformPvts2Vts( const char* dst, const char* src )
+{
+    std::cout << "reading " << src << " ..." << std::endl;
+    cvt::VtkXmlPStructuredGrid input_pvts( src, true );
+    std::cout << "#piece: " << input_pvts.number_of_pieces() << std::endl;
+
+    int count = 0;
+    for ( auto file_format : input_pvts )
+    {
+        std::cout << "importing ..." << std::endl;
+        cvt::VtkImporter<cvt::VtkXmlStructuredGrid> importer( file_format.get(),
+                                                              cvt::UNIFORM_GRID_MODE );
+        kvs::StructuredVolumeObject* object = &importer;
+        std::cout << "#nodes: " << object->numberOfNodes() << std::endl;
+        std::cout << "#resolution: [" << object->resolution()[0] << ", " << object->resolution()[1]
+                  << ", " << object->resolution()[2] << "]" << std::endl;
+
+        std::string output_filename =
+            std::string( dst ) + "." + std::to_string( count++ ) + ".kvsml";
+        std::cout << "writing " << output_filename << " ..." << std::endl;
+        kvs::StructuredVolumeExporter<kvs::KVSMLStructuredVolumeObject> exporter( &importer );
+        kvs::KVSMLStructuredVolumeObject* output_kvsml = &exporter;
+        output_kvsml->write( output_filename );
+    }
+}
