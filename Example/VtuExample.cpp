9320f05b85bf1cc33fff9c5476a874304d8bee21
Masayuki Suzuki <m-suzuki@advancesoft.jp>
2022-12-08 11:25:34 +0900

    Add a PFL IO

--- Example/VtuExample.cpp
+++ Example/VtuExample.cpp
@@ -5,6 +5,7 @@
 
 #include "Exporter/UnstructuredVolumeObjectExporter.h"
 #include "FileFormat/KVSML/KvsmlUnstructuredVolumeObject.h"
+#include "FileFormat/Pfl.h"
 #include "FileFormat/UnstructuredPfi.h"
 #include "FileFormat/VTK/VtkXmlUnstructuredGrid.h"
 #include "Importer/VtkImporter.h"
@@ -14,36 +15,41 @@ void Vtu2Kvsml( const char* directory, const char* base, const char* src )
     std::cout << "reading " << src << " ..." << std::endl;
     cvt::VtkXmlUnstructuredGrid input_vtu( src );
 
+    cvt::Pfl pfl;
+
     for ( auto vtu : input_vtu )
     {
+        int time_step = 0;
+        int last_time_step = 0;
+        int sub_volume_id = 1;
+        int sub_volume_count = 1;
+
         cvt::VtkImporter<cvt::VtkXmlUnstructuredGrid> importer( &vtu );
 
         kvs::UnstructuredVolumeObject* object = &importer;
         std::cout << "#nodes: " << object->numberOfNodes() << std::endl;
-        std::cout << "#cellType: " << object->cellType() << std::endl;
+        std::cout << "#cells: " << object->numberOfCells() << std::endl;
+        std::cout << "cellType: " << object->cellType() << std::endl;
 
         std::cout << "writing to " << directory << std::endl;
+        auto local_base = std::string( base ) + "_" + std::to_string( object->cellType() );
 
         cvt::UnstructuredVolumeObjectExporter exporter( &importer );
         exporter.setWritingDataTypeToExternalBinary();
-        exporter.writeForPbvr( directory, base,
-                               0, // time step
-                               1, // sub volume id
-                               1  // sub volume count
-        );
+        exporter.writeForPbvr( directory, local_base, time_step, sub_volume_id, sub_volume_count );
         // or
-        // exporter.write( "<directory/<base>_00000_0000001_0000001.kvsml" );
+        // exporter.write( "<directory/<local_base>_00000_0000001_0000001.kvsml" );
 
-        cvt::UnstructuredPfi pfi( object->veclen(),
-                                  0, // last time step
-                                  1  // sub volume count
-        );
-        pfi.registerObject( &exporter,
-                            0, // time step
-                            1  // sub volume id
-        );
-        pfi.write( directory, base );
+        cvt::UnstructuredPfi pfi( object->veclen(), last_time_step, sub_volume_count );
+        pfi.registerObject( &exporter, time_step, sub_volume_id );
+        pfi.write( directory, local_base );
         // or
-        // pfi.write( "<directory/<base>.pfi" );
+        // pfi.write( "<directory/<local_base>.pfi" );
+
+        pfl.registerPfi( directory, local_base );
     }
+
+    pfl.write( directory, base );
+    // or
+    // pfl.write( "<directory/<base>.pfi" );
 }
---
+++ Source/FileFormat/Pfl.h
@@ -0,0 +1,123 @@
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright (c) Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ */
+#ifndef CVT__PFL_H_INCLUDE
+#define CVT__PFL_H_INCLUDE
+
+#include <experimental/filesystem>
+#include <forward_list>
+#include <fstream>
+#include <string>
+
+namespace cvt
+{
+
+/**
+ * A PFL IO.
+ */
+class Pfl
+{
+public:
+    /**
+     * Construct a PFL IO.
+     */
+    Pfl() {}
+
+public:
+    /**
+     * Register a PFI file.
+     *
+     * \param [in] directory A directory name.
+     * \param [in] base A file name base.
+     */
+    template <typename PathLike0, typename PathLike1>
+    void registerPfi( PathLike0&& directory, PathLike1&& base )
+    {
+        std::experimental::filesystem::path path = directory;
+        path.make_preferred();
+        path /= ( std::string( base ) + ".pfi" );
+        pfi_list.push_front( path.string() );
+    }
+    /**
+     * Register a PFI file.
+     *
+     * \param [in] path A PFI file path.
+     */
+    template <typename PathLike>
+    void registerPfi( PathLike&& path )
+    {
+        pfi_list.push_front( std::string( path ) );
+    }
+    /**
+     * Write to a PFL file.
+     *
+     * The function will write to
+     * -    '<directory>/<base>.pfl' on POSIX or
+     * -    '<directory\<base>.pfl' on Windows.
+     *
+     * \param [in] directory A directory name.
+     * \param [in] base A file name base.
+     * \return `true` on success, otherwise `false`.
+     */
+    template <typename PathLike0, typename PathLike1>
+    bool write( PathLike0 directory, PathLike1 base )
+    {
+        try
+        {
+            std::experimental::filesystem::path path = directory;
+            path.make_preferred();
+            if ( !std::experimental::filesystem::exists( path ) )
+            {
+                std::experimental::filesystem::create_directories( path );
+            }
+
+            if ( !std::experimental::filesystem::exists( path ) )
+            {
+                throw std::runtime_error( "Failed to find the directory" );
+            }
+            path /= ( std::string( base ) + ".pfl" );
+
+            return write( path.c_str() );
+        }
+        catch ( std::exception& e )
+        {
+            throw e;
+            return false;
+        }
+    }
+    /**
+     * Write to a PFL file.
+     *
+     * \param [in] filename A file name.
+     * \return `true` on success, otherwise `false`.
+     */
+    bool write( const std::string& filename ) { return this->write( filename.c_str() ); }
+    /**
+     * Write to a PFL file.
+     *
+     * \param [in] filename A file name.
+     * \return `true` on success, otherwise `false`.
+     */
+    bool write( const char* filename )
+    {
+        std::ofstream stream( filename );
+
+        stream << "#PBVR PFI FILES" << std::endl;
+
+        for ( auto& p : pfi_list )
+        {
+            stream << p << std::endl;
+        }
+
+        return true;
+    }
+
+private:
+    std::forward_list<std::string> pfi_list;
+};
+} // namespace cvt
+
+#endif // CVT__PFL_H_INCLUDE
