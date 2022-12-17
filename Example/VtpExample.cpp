6568273324b35ba2c0a71fcebe4ab6c7875eaf71
Masayuki Suzuki <m-suzuki@advancesoft.jp>
2022-12-17 21:47:47 +0900

    Add new example to read a VTP file

---
+++ Example/VtpExample.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+#include "kvs/KVSMLPolygonObject"
+#include "kvs/PolygonExporter"
+#include "kvs/PolygonObject"
+
+#include "Exporter/VtkExporter.h"
+#include "FileFormat/VTK/VtkXmlPolyData.h"
+#include "Importer/VtkImporter.h"
+
+void Vtp2Kvsml( const char* dst, const char* src )
+{
+    std::cout << "reading " << src << " ..." << std::endl;
+    cvt::VtkXmlPolyData input_vtp( src );
+    cvt::VtkImporter<cvt::VtkXmlPolyData> importer( &input_vtp );
+
+    kvs::PolygonObject* polygon_object = &importer;
+    polygon_object->print( std::cout, kvs::Indent( 2 ) );
+
+    std::cout << "writing " << dst << " ..." << std::endl;
+    kvs::PolygonExporter<kvs::KVSMLPolygonObject> exporter( &importer );
+    exporter.write( dst );
+}
