3d1d425fae62d86c51b7f365f6bf70745781cbbd
Masayuki Suzuki <m-suzuki@advancesoft.jp>
2022-12-18 17:41:53 +0900

    Add generators of unstructured grid examples

---
+++ Example/Input/ExampleGenerator/common.py
@@ -0,0 +1,222 @@
+#!/usr/bin/python
+import vtk
+import math
+
+
+TETRA_BOTTOM_IDS = [
+    [0, 1, 3],
+    [1, 2, 3],
+    [0, 1, 4],
+    [1, 5, 4],
+    [1, 2, 5],
+    [2, 6, 5],
+    [3, 0, 4],
+    [3, 4, 7],
+    [2, 7, 3],
+    [2, 6, 7],
+    [4, 7, 5],
+    [7, 6, 5]
+]
+
+PRISM_BOTTOM_IDS = [
+    [0, 1, 2, 3],
+    [0, 1, 5, 4],
+    [1, 2, 6, 5],
+    [2, 3, 7, 6],
+    [3, 0, 4, 7],
+    [5, 4, 7, 6]
+]
+
+WEDGE_IDS = [
+    [4, 5, 7, 0, 1, 3],
+    [5, 7, 6, 1, 3, 2]
+]
+
+
+def append_point_data(unstructured_grid, offset=0):
+    dist = vtk.vtkFloatArray()
+    dist.SetNumberOfTuples(unstructured_grid.GetNumberOfPoints())
+    dist.SetNumberOfComponents(1)
+    dist.SetName('distance')
+
+    for i in range(unstructured_grid.GetNumberOfPoints()):
+        p = unstructured_grid.GetPoint(i)
+        dist.SetTuple(
+            i, [math.sqrt(p[0] * p[0] + p[1] * p[1] + p[2] * p[2]) + offset])
+
+    unstructured_grid.GetPointData().AddArray(dist)
+
+
+def make_hexahedron_only_mesh(dimensions, origin=[0, 0, 0], time_step=0):
+    image_data = vtk.vtkImageData()
+    image_data.SetDimensions(dimensions)
+    image_data.SetOrigin(origin)
+
+    itos = vtk.vtkImageToStructuredGrid()
+    itos.SetInputData(image_data)
+    itos.Update()
+
+    structured_grid = itos.GetOutput()
+
+    unstructured_grid = vtk.vtkUnstructuredGrid()
+    coords = []
+
+    for i in range(image_data.GetNumberOfPoints()):
+        c = structured_grid.GetPoint(i)
+        coords.append([c[0], c[1], c[2]])
+
+    for i in range(image_data.GetNumberOfCells()):
+        cell = structured_grid.GetCell(i)
+
+        unstructured_grid.InsertNextCell(
+            vtk.VTK_HEXAHEDRON, cell.GetPointIds())
+
+    points = vtk.vtkPoints()
+    points.SetNumberOfPoints(len(coords))
+    for i, c in enumerate(coords):
+        points.SetPoint(i, c[0], c[1], c[2])
+
+    unstructured_grid.SetPoints(points)
+
+    append_point_data(unstructured_grid, time_step)
+
+    return unstructured_grid
+
+
+def make_tetrahedron_only_mesh(dimensions, origin=[0, 0, 0], time_step=0):
+    image_data = vtk.vtkImageData()
+    image_data.SetDimensions(dimensions)
+    image_data.SetOrigin(origin)
+
+    itos = vtk.vtkImageToStructuredGrid()
+    itos.SetInputData(image_data)
+    itos.Update()
+
+    structured_grid = itos.GetOutput()
+
+    unstructured_grid = vtk.vtkUnstructuredGrid()
+    coords = []
+
+    for i in range(image_data.GetNumberOfPoints()):
+        c = structured_grid.GetPoint(i)
+        coords.append([c[0], c[1], c[2]])
+
+    for i in range(image_data.GetNumberOfCells()):
+        cell = structured_grid.GetCell(i)
+        b = vtk.vtkBoundingBox(cell.GetBounds())
+        center = [0.0, 0.0, 0.0]
+        b.GetCenter(center)
+        center_id = len(coords)
+        coords.append(center)
+
+        for h in TETRA_BOTTOM_IDS:
+            point_ids = vtk.vtkIdList()
+            point_ids.SetNumberOfIds(4)
+            for j, x in enumerate(h):
+                point_ids.SetId(j, cell.GetPointId(x))
+
+            point_ids.SetId(3, center_id)
+
+            unstructured_grid.InsertNextCell(
+                vtk.VTK_TETRA, point_ids)
+
+    points = vtk.vtkPoints()
+    points.SetNumberOfPoints(len(coords))
+    for i, c in enumerate(coords):
+        points.SetPoint(i, c[0], c[1], c[2])
+
+    unstructured_grid.SetPoints(points)
+
+    append_point_data(unstructured_grid, time_step)
+
+    return unstructured_grid
+
+
+def make_pyramid_only_mesh(dimensions, origin=[0, 0, 0], time_step=0):
+    image_data = vtk.vtkImageData()
+    image_data.SetDimensions(dimensions)
+    image_data.SetOrigin(origin)
+
+    itos = vtk.vtkImageToStructuredGrid()
+    itos.SetInputData(image_data)
+    itos.Update()
+
+    structured_grid = itos.GetOutput()
+
+    unstructured_grid = vtk.vtkUnstructuredGrid()
+    coords = []
+
+    for i in range(image_data.GetNumberOfPoints()):
+        c = structured_grid.GetPoint(i)
+        coords.append([c[0], c[1], c[2]])
+
+    for i in range(image_data.GetNumberOfCells()):
+        cell = structured_grid.GetCell(i)
+        b = vtk.vtkBoundingBox(cell.GetBounds())
+        center = [0.0, 0.0, 0.0]
+        b.GetCenter(center)
+        center_id = len(coords)
+        coords.append(center)
+
+        for h in PRISM_BOTTOM_IDS:
+            point_ids = vtk.vtkIdList()
+            point_ids.SetNumberOfIds(5)
+            for j, x in enumerate(h):
+                point_ids.SetId(j, cell.GetPointId(x))
+
+            point_ids.SetId(4, center_id)
+
+            unstructured_grid.InsertNextCell(
+                vtk.VTK_PYRAMID, point_ids)
+
+    points = vtk.vtkPoints()
+    points.SetNumberOfPoints(len(coords))
+    for i, c in enumerate(coords):
+        points.SetPoint(i, c[0], c[1], c[2])
+
+    unstructured_grid.SetPoints(points)
+
+    append_point_data(unstructured_grid, time_step)
+
+    return unstructured_grid
+
+
+def make_wedge_only_mesh(dimensions, origin=[0, 0, 0], time_step=0):
+    image_data = vtk.vtkImageData()
+    image_data.SetDimensions(dimensions)
+    image_data.SetOrigin(origin)
+
+    itos = vtk.vtkImageToStructuredGrid()
+    itos.SetInputData(image_data)
+    itos.Update()
+
+    structured_grid = itos.GetOutput()
+
+    unstructured_grid = vtk.vtkUnstructuredGrid()
+    coords = []
+
+    for i in range(image_data.GetNumberOfPoints()):
+        c = structured_grid.GetPoint(i)
+        coords.append([c[0], c[1], c[2]])
+
+    for i in range(image_data.GetNumberOfCells()):
+        cell = structured_grid.GetCell(i)
+
+        for h in WEDGE_IDS:
+            point_ids = vtk.vtkIdList()
+            point_ids.SetNumberOfIds(6)
+            for j, x in enumerate(h):
+                point_ids.SetId(j, cell.GetPointId(x))
+
+            unstructured_grid.InsertNextCell(vtk.VTK_WEDGE, point_ids)
+
+    points = vtk.vtkPoints()
+    points.SetNumberOfPoints(len(coords))
+    for i, c in enumerate(coords):
+        points.SetPoint(i, c[0], c[1], c[2])
+
+    unstructured_grid.SetPoints(points)
+
+    append_point_data(unstructured_grid, time_step)
+
+    return unstructured_grid
---
+++ Example/Input/ExampleGenerator/unstructuredgrid.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+import vtk
+import os
+from common import (
+    TETRA_BOTTOM_IDS,
+    make_hexahedron_only_mesh,
+    make_tetrahedron_only_mesh,
+    make_pyramid_only_mesh,
+    make_wedge_only_mesh,
+    append_point_data)
+
+
+def generate_hexahedron_only_mesh():
+    for time_step in range(10):
+        unstructured_grid = make_hexahedron_only_mesh(
+            [3, 4, 5], [0, 0, 0], time_step)
+
+        writer = vtk.vtkXMLUnstructuredGridWriter()
+        writer.SetFileName(os.path.join('..', 'Hex',
+                           f'hex_only_{time_step}.vtu'))
+        writer.SetInputData(unstructured_grid)
+        writer.Update()
+
+
+def generate_tetrahedron_only_mesh():
+    for time_step in range(10):
+        unstructured_grid = make_tetrahedron_only_mesh(
+            [3, 4, 5], [0, 0, 0], time_step)
+
+        writer = vtk.vtkXMLUnstructuredGridWriter()
+        writer.SetFileName(os.path.join('..', 'Tetra',
+                           f'tetra_only_{time_step}.vtu'))
+        writer.SetInputData(unstructured_grid)
+        writer.Update()
+
+
+def generate_tetrahedron_and_hexahedron():
+    for time_step in range(10):
+        image_data = vtk.vtkImageData()
+        image_data.SetDimensions([3, 4, 5])
+
+        itos = vtk.vtkImageToStructuredGrid()
+        itos.SetInputData(image_data)
+        itos.Update()
+
+        structured_grid = itos.GetOutput()
+
+        unstructured_grid = vtk.vtkUnstructuredGrid()
+        coords = []
+
+        for i in range(image_data.GetNumberOfPoints()):
+            c = structured_grid.GetPoint(i)
+            coords.append([c[0], c[1], c[2]])
+
+        for i in range(image_data.GetNumberOfCells()):
+            cell = structured_grid.GetCell(i)
+
+            if i > image_data.GetNumberOfCells() / 2:
+                b = vtk.vtkBoundingBox(cell.GetBounds())
+                center = [0.0, 0.0, 0.0]
+                b.GetCenter(center)
+                center_id = len(coords)
+                coords.append(center)
+
+                for h in TETRA_BOTTOM_IDS:
+                    point_ids = vtk.vtkIdList()
+                    point_ids.SetNumberOfIds(4)
+                    for j, x in enumerate(h):
+                        point_ids.SetId(j, cell.GetPointId(x))
+
+                    point_ids.SetId(3, center_id)
+
+                    unstructured_grid.InsertNextCell(
+                        vtk.VTK_TETRA, point_ids)
+            else:
+                unstructured_grid.InsertNextCell(
+                    vtk.VTK_HEXAHEDRON, cell.GetPointIds())
+
+        points = vtk.vtkPoints()
+        points.SetNumberOfPoints(len(coords))
+        for i, c in enumerate(coords):
+            points.SetPoint(i, c[0], c[1], c[2])
+
+        unstructured_grid.SetPoints(points)
+
+        append_point_data(unstructured_grid, time_step)
+
+        writer = vtk.vtkXMLUnstructuredGridWriter()
+        writer.SetFileName(os.path.join(
+            '..', 'TetraAndHex', f'tetra_and_hex_{time_step}.vtu'))
+        writer.SetInputData(unstructured_grid)
+        writer.Update()
+
+
+def generate_multi_block():
+    for time_step in range(10):
+        multi_block = vtk.vtkMultiBlockDataSet()
+        hex_only = make_hexahedron_only_mesh([3, 4, 5], [0, 0, 0], time_step)
+        tetra_only = make_tetrahedron_only_mesh(
+            [3, 4, 5], [2, 0, 0], time_step)
+        tetra_only = make_tetrahedron_only_mesh(
+            [3, 4, 5], [2, 0, 0], time_step)
+        pyramid_only = make_pyramid_only_mesh(
+            [3, 4, 5], [4, 0, 0], time_step)
+        wedge_only = make_wedge_only_mesh(
+            [3, 4, 5], [6, 0, 0], time_step)
+
+        multi_block.SetBlock(0, hex_only)
+        multi_block.SetBlock(1, tetra_only)
+        multi_block.SetBlock(2, pyramid_only)
+        multi_block.SetBlock(3, wedge_only)
+
+        writer = vtk.vtkXMLMultiBlockDataWriter()
+        writer.SetFileName(os.path.join('..', 'MultiBlock',
+                           f'multiblock_{time_step}.vtm'))
+        writer.SetInputData(multi_block)
+        writer.Update()
+
+
+if __name__ == '__main__':
+    generate_tetrahedron_only_mesh()
+    generate_hexahedron_only_mesh()
+    generate_tetrahedron_and_hexahedron()
+    generate_multi_block()
