77ed80fd60fa978a3e76e867ee6236aef4d67f12
Masayuki Suzuki <m-suzuki@advancesoft.jp>
2023-01-24 18:03:06 +0900

    Fix license headers

--- Example/Plot3dExample.cpp
+++ Example/Plot3dExample.cpp
@@ -1,3 +1,13 @@
+/*
+ * Created by Japan Atomic Energy Agency
+ *
+ * To the extent possible under law, the person who associated CC0 with
+ * this file has waived all copyright and related or neighboring rights
+ * to this file.
+ *
+ * You should have received a copy of the CC0 legal code along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
 #include <iostream>
 
 #include "kvs/StructuredVolumeObject"
--- Source/TimeSeriesFiles/EnSight/EnSightGold.h
+++ Source/TimeSeriesFiles/EnSight/EnSightGold.h
@@ -1,8 +1,18 @@
-/*----------------------------------------------------------------------------
+/*
+ * Copyright (c) 2022 Japan Atomic Energy Agency
  *
- *  Copyright (c) Visualization Laboratory, Kyoto University.
- *  All rights reserved.
- *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 #ifndef CVT__ENSIGHT_GOLD_H_INCLUDE
 #define CVT__ENSIGHT_GOLD_H_INCLUDE
