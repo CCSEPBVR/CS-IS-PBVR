0d8365767324bd59358e3fefaea1dc7a3699f3d3
T0603 <t0603@TOMacBook.edo.jaea.go.jp>
2023-11-21 14:22:40 +0900

    QTPBVR向けQtScreenを実装

--- ExtendedKVS/QtScreen.cpp
+++ ExtendedKVS/QtScreen.cpp
@@ -1,6 +1,27 @@
 #include "QtScreen.h"
 
-QtScreen::QtScreen()
+QtScreen::QtScreen( kvs::qt::Application* application, QWidget* parent ):
+    kvs::qt::Screen( application, parent )
 {
+}
 
+/*===========================================================================*/
+/**
+ *  @brief  Key press event.
+ *  @param  event [in] pointer to the key event information
+ */
+/*===========================================================================*/
+void QtScreen::keyPressEvent( kvs::KeyEvent* event )
+{
+    if( !this->scene() ) { return; }
+
+    switch( event->key() )
+    {
+    case kvs::Key::Home:
+        this->scene()->reset();
+        this->update();
+        break;
+    default:
+        break;
+    }
 }
--- ExtendedKVS/QtScreen.h
+++ ExtendedKVS/QtScreen.h
@@ -1,11 +1,16 @@
 #ifndef QTSCREEN_H
 #define QTSCREEN_H
 
+#include <kvs/qt/Screen>
 
-class QtScreen
+class QtScreen : public kvs::qt::Screen
 {
 public:
-    QtScreen();
+    QtScreen( kvs::qt::Application* application = 0, QWidget* parent = 0 );
+
+    virtual void mouseReleaseEvent( kvs::MouseEvent* event ){ Q_UNUSED( event ); }
+    virtual void wheelEvent( kvs::WheelEvent* event ){ Q_UNUSED( event ); }
+    virtual void keyPressEvent( kvs::KeyEvent* event );
 };
 
 #endif // QTSCREEN_H
