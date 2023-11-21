de46945486a1a6adf24ddb1ee539fbaa7125ca0f
T0603 <t0603@TOMacBook.edo.jaea.go.jp>
2023-11-21 10:26:28 +0900

    uiの命名規則 を変更

--- Panels/VolumeTransform.cpp
+++ Panels/VolumeTransform.cpp
@@ -8,8 +8,8 @@ VolumeTransform::VolumeTransform(QWidget *parent) :
     ui(new Ui::VolumeTransform)
 {
     ui->setupUi(this);
-
-    connect( ui->applyBtn, &QPushButton::clicked, this, &VolumeTransform::onApplyButtonClicked );
+    
+    connect( ui->applyPBtn, &QPushButton::clicked, this, &VolumeTransform::onApplyButtonClicked );
 }
 
 VolumeTransform::~VolumeTransform()
@@ -21,23 +21,21 @@ void VolumeTransform::onApplyButtonClicked()
 {
     m_screen->reset();
     kvs::Vector3f translate(
-                ui->translationXAxisSB->value(),
-                ui->translationYAxisSB->value(),
-                ui->translationZAxisSB->value()
-                );
+        ui->translationXAxisDSBox->value(),
+        ui->translationYAxisDSBox->value(),
+        ui->translationZAxisDSBox->value() );
     m_screen->scene()->objectManager()->translate( translate );
 
     kvs::Vector3f scaling(
-                ui->scaleSB->value(),
-                ui->scaleSB->value(),
-                ui->scaleSB->value()
-                );
+        ui->scaleDSBox->value(),
+        ui->scaleDSBox->value(),
+        ui->scaleDSBox->value() );
     m_screen->scene()->objectManager()->scale( scaling );
 
     m_screen->scene()->objectManager()->rotate( kvs::RPYRotationMatrix33<float>(
-                                                    ui->rotationXAxisSB->value(),
-                                                    ui->rotationYAxisSB->value(),
-                                                    ui->rotationZAxisSB->value()
-                                                    ) );
+        ui->rotationXAxisDSBox->value(),
+        ui->rotationYAxisDSBox->value(),
+        ui->rotationZAxisDSBox->value()) );
+
     m_screen->update();
 }
