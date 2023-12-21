62bbdf6f4bc968f857507ef105eb167d0144b821
T0603 <t0603@TOMacBook.edo.jaea.go.jp>
2023-12-21 10:19:45 +0900

    リファクタリング

--- Widgets/Merge.h
+++ Widgets/Merge.h
@@ -24,33 +24,33 @@ public:
         LASPointObject     = 5, // .las
     };
 
-    void setFileInfo( QFileInfo fileInfo )      { m_file_info       = fileInfo;        }
-    void setFileName(QString file_name){ m_file_name = file_name; }
+    void setFileInfo( QFileInfo fileInfo ) { m_file_info = fileInfo; }
+    void setFileName(QString file_name) { m_file_name = file_name; }
     void setMinTimeStep(int min_time_step) { m_min_time_step = min_time_step; }
     void setMaxTimeStep(int max_time_step) { m_max_time_step = max_time_step; }
 
-    void setVisible( Qt::CheckState is_visible )          { m_is_visible      = is_visible;      }
+    void setVisible( Qt::CheckState is_visible ) { m_is_visible = is_visible; }
     void setKeepInitial( Qt::CheckState is_keep_initial ) { m_is_keep_initial = is_keep_initial; }
-    void setKeepFinal( Qt::CheckState is_keep_final )     { m_is_keep_final   = is_keep_final;   }
-    void setFileSuffix( QString file_suffix )       { m_file_suffix       = file_suffix;       }
-    void setFileFormat( FileType file_format )   { m_file_format     = file_format;     }
-    void setRGBColor( QColor rgb_color ) { m_rgb_color       = rgb_color;       }
-    void setOpacity( double opacity )           { m_opacity         = opacity;         }
+    void setKeepFinal( Qt::CheckState is_keep_final ) { m_is_keep_final = is_keep_final; }
+    void setFileSuffix( QString file_suffix ) { m_file_suffix = file_suffix; }
+    void setFileFormat( FileType file_format ) { m_file_format = file_format; }
+    void setRGBColor( QColor rgb_color ) { m_rgb_color = rgb_color; }
+    void setOpacity( double opacity ) { m_opacity = opacity; }
     void setIds( std::pair<int, int> ids ) { m_ids = ids; }
     void setIsModified( bool is_modified ) { m_is_modified = is_modified; }
 
-    QFileInfo getFileInfo()     { return m_file_info;       }
+    QFileInfo getFileInfo() { return m_file_info; }
     QString getFileName() const {return m_file_name; }
-    int getMinTimeStep() const  { return m_min_time_step; }
-    int getMaxTimeStep() const  { return m_max_time_step; }
+    int getMinTimeStep() const { return m_min_time_step; }
+    int getMaxTimeStep() const { return m_max_time_step; }
 
-    Qt::CheckState getVisible()           { return m_is_visible;      }
-    Qt::CheckState getKeepInitial()       { return m_is_keep_initial; }
-    Qt::CheckState getKeepFinal()         { return m_is_keep_final;   }
-    QString getFileSuffix()       { return m_file_suffix;       }
-    FileType getFileFormat()     { return m_file_format;     }
-    QColor getRGBColor() { return m_rgb_color;       }
-    double getOpacity()         { return m_opacity;         }
+    Qt::CheckState getVisible() { return m_is_visible; }
+    Qt::CheckState getKeepInitial() { return m_is_keep_initial; }
+    Qt::CheckState getKeepFinal() { return m_is_keep_final; }
+    QString getFileSuffix() { return m_file_suffix; }
+    FileType getFileFormat() { return m_file_format; }
+    QColor getRGBColor() { return m_rgb_color; }
+    double getOpacity() { return m_opacity; }
     std::pair<int,int> getIds() { return m_ids; }
     bool getIsModified() { return m_is_modified; }
 
@@ -108,8 +108,8 @@ private:
 
     void onBrowserButtonClicked();
     void onAddButtonClicked();
-    void checkMinMaxTimeStep(QFileInfo *fileInfo, QDir *directory,FilesManager *filesManager);
-    void checkFileFormat(QFileInfo *fileInfo, FilesManager *filesManager);
+    void checkMinMaxTimeStep( QFileInfo *fileInfo, QDir *directory, FilesManager *filesManager );
+    void checkFileFormat( QFileInfo *fileInfo, FilesManager *filesManager );
     void onApplyButtonClicked();
     void removeChecker();
     void registerFile( FilesManager* filesManager );
@@ -118,16 +118,16 @@ private:
     void mergeObjects();
 
     template <typename Importer, typename ObjectType>
-    ObjectType* selectPattern(FilesManager* filesManager);
+    ObjectType* selectPattern( FilesManager* filesManager );
     template <typename Importer, typename ObjectType>
-    ObjectType* timeStepCheckAndImport(FilesManager* filesManager, pattern pattern);
+    ObjectType* timeStepCheckAndImport( FilesManager* filesManager, pattern pattern );
 
-    kvs::PointObject* selectPattern(FilesManager* filesManager);
-    kvs::PointObject* timeStepCheckAndImport(FilesManager* filesManager, pattern pattern);
+    kvs::PointObject* selectPattern( FilesManager* filesManager );
+    kvs::PointObject* timeStepCheckAndImport( FilesManager* filesManager, pattern pattern );
 
     QString updateTimeStepInFileName(QString fileName,int nextTimeStep);
-    void removeObject(FilesManager* filesManager);
-    void updateObject(FilesManager* filesManager, kvs::ObjectBase* object);
+    void removeObject( FilesManager* filesManager );
+    void updateObject( FilesManager* filesManager, kvs::ObjectBase* object );
     void onFilesTWidgetCellDoubleClicked(int row, int column);        
     void showFilesManager();
 };
