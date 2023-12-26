525bcf9c0f01351d85c437aa71cb9375e7102e93
T0603 <t0603@TOMacBook.edo.jaea.go.jp>
2023-12-26 14:10:11 +0900

    ColorMapBarのプロトタイプを作成

--- Widgets/TransferFunctionEditor.cpp
+++ Widgets/TransferFunctionEditor.cpp
@@ -1,15 +1,19 @@
 #include "TransferFunctionEditor.h"
 #include "ui_TransferFunctionEditor.h"
 #include <kvs/ColorMapBar>
+#include <kvs/OpacityMapBar>
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
-class TriangleRenderer : public QOpenGLWidget, protected QOpenGLFunctions
+class TFColorMapBar : public QOpenGLWidget, protected QOpenGLFunctions
 {
 public:
-    TriangleRenderer(QWidget *parent = nullptr) : QOpenGLWidget(parent)
+    TFColorMapBar(QWidget *parent = nullptr) : QOpenGLWidget(parent)
     {
         this->setMinimumHeight(80);
         this->setMaximumHeight(80);
+
+        m_colormap.setResolution( 256 );
+        m_colormap.create();
     }
 
 protected:
@@ -21,23 +25,50 @@ protected:
 
     void paintGL() override
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        if( !m_texture_downloaded )
+        {
+            const size_t nchannels = 3;
+            const size_t width = m_colormap.resolution();
+            const size_t height = 1;
+            const kvs::UInt8* data = m_colormap.table().data();
 
-        // Draw a simple triangle
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 0.0f, 0.0f); // Red color
-        glVertex3f(0.0f, 1.0f, 0.0f); // Top vertex
-        glColor3f(0.0f, 1.0f, 0.0f); // Green color
-        glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom-left vertex
-        glColor3f(0.0f, 0.0f, 1.0f); // Blue color
-        glVertex3f(1.0f, -1.0f, 0.0f); // Bottom-right vertex
-        glEnd();
+            m_texture.release();
+            m_texture.setPixelFormat( nchannels, sizeof( kvs::UInt8 ) );
+            m_texture.setMinFilter( GL_NEAREST );
+            m_texture.setMagFilter( GL_NEAREST );
+            m_texture.create( width, height, data );
+            m_texture_downloaded = true;
+        }
+
+        kvs::OpenGL::WithPushedAttrib attrib( GL_ALL_ATTRIB_BITS );
+        attrib.disable( GL_BLEND );
+        attrib.disable( GL_DEPTH_TEST );
+        attrib.disable( GL_TEXTURE_3D );
+        attrib.enable( GL_TEXTURE_2D );
+
+        const kvs::Vec2 p0 = kvs::Vec2( -1, -1 );
+        const kvs::Vec2 p1 = kvs::Vec2(  1, -1 );
+        const kvs::Vec2 p2 = kvs::Vec2(  1,  1 );
+        const kvs::Vec2 p3 = kvs::Vec2( -1,  1 );
+
+        kvs::Texture::Binder binder( m_texture );
+
+        kvs::OpenGL::Begin( GL_QUADS );
+        kvs::OpenGL::TexCoordVertex( kvs::Vec2( 0.0f, 1.0f ), p0 );
+        kvs::OpenGL::TexCoordVertex( kvs::Vec2( 1.0f, 1.0f ), p1 );
+        kvs::OpenGL::TexCoordVertex( kvs::Vec2( 1.0f, 0.0f ), p2 );
+        kvs::OpenGL::TexCoordVertex( kvs::Vec2( 0.0f, 0.0f ), p3 );
+        kvs::OpenGL::End();
     }
 
     void resizeGL(int w, int h) override
     {
         glViewport(0, 0, w, h);
     }
+private:
+    kvs::ColorMap m_colormap; ///< color map
+    kvs::Texture2D m_texture; ///< texture data
+    bool m_texture_downloaded; ///< check flag for texture
 };
 
 TransferFunctionEditor::TransferFunctionEditor(QWidget *parent) :
@@ -45,15 +76,10 @@ TransferFunctionEditor::TransferFunctionEditor(QWidget *parent) :
     ui(new Ui::TransferFunctionEditor)
 {
     ui->setupUi(this);
-    TriangleRenderer* tr1 = new TriangleRenderer();
-    TriangleRenderer* tr2 = new TriangleRenderer();
-    TriangleRenderer* tr3 = new TriangleRenderer();
-    TriangleRenderer* tr4 = new TriangleRenderer();
+    TFColorMapBar* tr1 = new TFColorMapBar();    
+;
 
-    ui->colorMapBarGLayout->addWidget( tr1, 0,0,1,1 );
-    ui->colorMapHistogramGLayout->addWidget( tr2, 0,0,1,1 );
-    ui->opacityMapBarGLayout->addWidget( tr3, 0,0,1,1 );
-    ui->opacityMapHistogramGLayout->addWidget( tr4, 0,0,1,1 );
+    ui->colorMapBarGLayout->addWidget( tr1, 0,0,1,1 );    
 }
 
 TransferFunctionEditor::~TransferFunctionEditor()
