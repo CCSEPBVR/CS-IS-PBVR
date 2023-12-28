38bfa8d1dd46896cea0842328c2a9ecfe703cab0
T0603 <t0603@TOMacBook.edo.jaea.go.jp>
2023-12-28 17:21:11 +0900

    HistogramBarを実装

--- Widgets/TFEHistogramBar.cpp
+++ Widgets/TFEHistogramBar.cpp
@@ -1,8 +1,11 @@
 #include "TFEHistogramBar.h"
+#include <QMouseEvent>
 
 TFEHistogramBar::TFEHistogramBar(QWidget *parent)
-    :QOpenGLWidget(parent)
+    :QOpenGLWidget(parent),
+    m_bias_parameter(0.5)
 {
+    m_graph_color = kvs::RGBAColor( 128, 128, 128, 1 );
     m_table.setNumberOfBins( 256 );
 }
 
@@ -10,56 +13,133 @@ TFEHistogramBar::~TFEHistogramBar()
 {
 }
 
+void TFEHistogramBar::create( const kvs::VolumeObjectBase* volume )
+{
+    volume->updateMinMaxValues();
+    m_table.create( volume );
+}
+
 void TFEHistogramBar::initializeGL()
 {
     initializeOpenGLFunctions();
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // Set clear color to white
+    kvs::OpenGL::SetClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 }
 
 void TFEHistogramBar::paintGL()
 {
-    //create_texture
-    const size_t nchannels = 4;
-    const size_t width = m_table.numberOfBins();
-    const size_t height = width;
+    if ( !m_texture.isValid() ) this->create_texture();
 
-    m_texture.release();
-    m_texture.setPixelFormat( nchannels, sizeof( kvs::UInt8 ) );
-    m_texture.setMinFilter( GL_LINEAR );
-    m_texture.setMagFilter( GL_LINEAR );
-    m_texture.create( width, height, this->get_histogram_image().data() );
+    //    FROM OLD QTPBVR qglue_base begin_draw
+    //    GLint vp[4]; glGetIntegerv( GL_VIEWPORT, vp );
+    //    glPushAttrib( GL_ALL_ATTRIB_BITS );
+    //    glMatrixMode( GL_MODELVIEW );  glPushMatrix(); glLoadIdentity();
+    //    glMatrixMode( GL_PROJECTION ); glPushMatrix(); glLoadIdentity();
+    //    glOrtho( left, right, top, bottom, -1, 1 ); // The origin is upper-left.
+    //    glDisable( GL_DEPTH_TEST );
+    GLint vp[4];
+    kvs::OpenGL::GetIntegerv( GL_VIEWPORT, vp );
+    const GLint left   = vp[0];
+    const GLint bottom = vp[1];
+    const GLint right  = vp[2];
+    const GLint top    = vp[3];
 
-    //draw_palette
+    kvs::OpenGL::WithPushedAttrib attrib( GL_ALL_ATTRIB_BITS );
+    kvs::OpenGL::SetMatrixMode( GL_MODELVIEW );
+    kvs::OpenGL::PushMatrix();
+    kvs::OpenGL::LoadIdentity();
+
+    kvs::OpenGL::SetMatrixMode( GL_PROJECTION );
+    kvs::OpenGL::PushMatrix();
+    kvs::OpenGL::LoadIdentity();
+
+    kvs::OpenGL::SetOrtho( left, right, top, bottom );
+    kvs::OpenGL::Disable( GL_DEPTH_TEST );
+
+    this->draw_palette();
+
+    //    FROM OLD QTPBVR qglue_base
+    //    glPopMatrix();
+    //    glMatrixMode( GL_MODELVIEW );
+    //    glPopMatrix();
+    //    glPopAttrib();
+    kvs::OpenGL::PopMatrix();
+    kvs::OpenGL::SetMatrixMode( GL_MODELVIEW );
+    kvs::OpenGL::PopMatrix();
+    kvs::OpenGL::PopAttrib();
+}
+
+void TFEHistogramBar::resizeGL(int w, int h)
+{
+    int h_scaled = h * this->devicePixelRatio();
+    int w_scaled = w * this->devicePixelRatio();
+
+    m_palette.setRect(0, 0, w_scaled, h_scaled);
+}
+
+void TFEHistogramBar::mousePressEvent( QMouseEvent *event )
+{
+    // Current mouse cursor position.
+    const int x = event->x() * this->devicePixelRatio();
+    const int y = event->y() * this->devicePixelRatio();
+    m_previous_position.set( x, y );
+
+    update();
+}
+
+void TFEHistogramBar::mouseMoveEvent( QMouseEvent *event )
+{
+    const int x = event->x() * this->devicePixelRatio();
+    const int y = event->y() * this->devicePixelRatio();
+    const int dy = y - m_previous_position.y();
+
+    // Calculate the bias parameter.
+    m_bias_parameter -= dy * 0.005f;
+    m_bias_parameter = kvs::Math::Clamp( m_bias_parameter, 0.0f, 0.99999f );
+    this->update_texture();
+
+    m_previous_position.set( x, y );
+
+    update();
+}
+
+void TFEHistogramBar::mouseReleaseEvent( QMouseEvent *event )
+{
+    Q_UNUSED( event );
+}
+
+void TFEHistogramBar::draw_palette()
+{
     kvs::OpenGL::WithPushedAttrib attrib( GL_ALL_ATTRIB_BITS );
     attrib.disable( GL_TEXTURE_1D );
     attrib.enable( GL_TEXTURE_2D );
     attrib.disable( GL_TEXTURE_3D );
 
+    const int x0 = m_palette.x();
+    const int x1 = m_palette.x() + m_palette.width();
+    const int y0 = m_palette.y();
+    const int y1 = m_palette.y() + m_palette.height();
+
     kvs::OpenGL::Begin( GL_QUADS );
     kvs::OpenGL::Color( kvs::RGBColor( 230, 230, 230 ) );
-    kvs::OpenGL::Vertex( kvs::Vec2( -1, -1 ) );
-    kvs::OpenGL::Vertex( kvs::Vec2( 1, -1 ) );
-    kvs::OpenGL::Vertex( kvs::Vec2( 1, 1 ) );
-    kvs::OpenGL::Vertex( kvs::Vec2( -1, 1 ) );
+    kvs::OpenGL::Vertex( kvs::Vec2( x0, y0 ) );
+    kvs::OpenGL::Vertex( kvs::Vec2( x1, y0 ) );
+    kvs::OpenGL::Vertex( kvs::Vec2( x1, y1 ) );
+    kvs::OpenGL::Vertex( kvs::Vec2( x0, y1 ) );
     kvs::OpenGL::End();
 
     kvs::OpenGL::Enable( GL_BLEND );
     kvs::OpenGL::SetBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
-
-    kvs::Texture::Binder binder( m_texture );
-    kvs::OpenGL::Begin( GL_QUADS );
-    kvs::OpenGL::TexCoordVertex( kvs::Vec2( 0.0f, 0.0f ), kvs::Vec2( -1, -1 ) );
-    kvs::OpenGL::TexCoordVertex( kvs::Vec2( 1.0f, 0.0f ), kvs::Vec2( 1, -1 ) );
-    kvs::OpenGL::TexCoordVertex( kvs::Vec2( 1.0f, 1.0f ), kvs::Vec2( 1, 1 ) );
-    kvs::OpenGL::TexCoordVertex( kvs::Vec2( 0.0f, 1.0f ), kvs::Vec2( -1, 1 ) );
-    kvs::OpenGL::End();
-
+    if ( m_texture.isValid() )
+    {
+        kvs::Texture::Binder binder( m_texture );
+        kvs::OpenGL::Begin( GL_QUADS );
+        kvs::OpenGL::TexCoordVertex( kvs::Vec2( 0.0f, 0.0f ), kvs::Vec2( x0, y1 ) );
+        kvs::OpenGL::TexCoordVertex( kvs::Vec2( 1.0f, 0.0f ), kvs::Vec2( x1, y1 ) );
+        kvs::OpenGL::TexCoordVertex( kvs::Vec2( 1.0f, 1.0f ), kvs::Vec2( x1, y0 ) );
+        kvs::OpenGL::TexCoordVertex( kvs::Vec2( 0.0f, 1.0f ), kvs::Vec2( x0, y0 ) );
+        kvs::OpenGL::End();
+    }
     kvs::OpenGL::Disable( GL_BLEND );
-}
-
-void TFEHistogramBar::resizeGL(int w, int h)
-{
-    glViewport(0, 0, w, h);
 }
 
 const kvs::ValueArray<kvs::UInt8> TFEHistogramBar::get_histogram_image() const
@@ -100,4 +180,22 @@ const kvs::ValueArray<kvs::UInt8> TFEHistogramBar::get_histogram_image() const
     }
 
     return data;
+}
+
+void TFEHistogramBar::create_texture()
+{
+    const size_t nchannels = 4;
+    const size_t width = m_table.numberOfBins();
+    const size_t height = width;
+
+    m_texture.release();
+    m_texture.setPixelFormat( nchannels, sizeof( kvs::UInt8 ) );
+    m_texture.setMinFilter( GL_LINEAR );
+    m_texture.setMagFilter( GL_LINEAR );
+    m_texture.create( width, height, this->get_histogram_image().data() );
+}
+
+void TFEHistogramBar::update_texture()
+{
+    this->create_texture();
 }
--- Widgets/TFEHistogramBar.h
+++ Widgets/TFEHistogramBar.h
@@ -15,15 +15,27 @@ protected:
     void initializeGL() override;
     void paintGL() override;
     void resizeGL(int w, int h) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
 
 private:
     kvs::FrequencyTable m_table; ///< frequency distribution table
     kvs::RGBAColor m_graph_color; ///< graph color
     float m_bias_parameter; ///< bias parameter
     kvs::Texture2D m_texture; ///< histogram texture
+    QRect m_palette; ///< palette
+    kvs::Vec2i m_previous_position; ///< mouse previous position
+
+public:
+    void create( const kvs::VolumeObjectBase* volume );
 
 private:
+    void draw_palette();
     const kvs::ValueArray<kvs::UInt8> get_histogram_image() const;
+    void create_texture();
+    void update_texture();
+
 };
 
 #endif // TFEHISTOGRAMBAR_H
