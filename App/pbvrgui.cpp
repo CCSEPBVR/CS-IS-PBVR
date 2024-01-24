3c1f0f4ccb39362a7ef74ebcbcfc4496715f8819
T0603 <t0603@TOMacBook.edo.jaea.go.jp>
2024-01-24 09:20:18 +0900

    TransferFunctionEditorにclient messageをセット

--- App/pbvrgui.cpp
+++ App/pbvrgui.cpp
@@ -100,6 +100,8 @@ PBVRGUI::PBVRGUI(kvs::qt::Application& app, QWidget *parent) :
     addDockWidget(Qt::LeftDockWidgetArea, &m_repetition_level_control);
     m_repetition_level_control.setScreen( m_screen );
 
+    m_transfer_function_editor.setClientMessage( m_connect.getClientMessage() );
+
     setFocusPolicy(Qt::StrongFocus);
 }
 
