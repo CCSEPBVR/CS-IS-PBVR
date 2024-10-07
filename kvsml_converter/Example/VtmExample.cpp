3888b1b9c1e1c90be5cd1ce668947c8c65af84c1
sakamoto-naohito <sakamoto.naohito@jaea.go.jp>
2024-10-07 16:38:45 +0900

    kvsml-converter exampleプログラム「Vtm2Kvsml」を実装

--- kvsml_converter/Example/VtmExample.cpp
+++ kvsml_converter/Example/VtmExample.cpp
@@ -80,6 +80,96 @@ void AccessToVtm( const std::string& src )
     }
 }
 
+void Vtm2Kvsml( const std::string& directory, const std::string& base,
+                      const std::string& src )
+{
+    std::cout << "Reading " << src << " ..." << std::endl;
+    std::unordered_map<int, cvt::UnstructuredPfi> pfi_map;
+
+    cvt::VtkXmlMultiBlock input_vtm( src );
+    int last_time_step = 0;
+    int time_step = 0;
+    int sub_volume_id = 1;
+    std::unordered_map<int, int> sub_volume_counts;
+    std::unordered_map<int, int> sub_volume_ids;
+    std::unordered_map<int, int> veclens;
+
+    for ( auto format : input_vtm.eachBlock() )
+    {
+        if ( auto input_vtu = dynamic_cast<cvt::VtkXmlUnstructuredGrid*>( format.get() ) )
+        {
+            for ( auto vtu : input_vtu->eachCellType() )
+            {
+                cvt::VtkImporter<cvt::VtkXmlUnstructuredGrid> importer( input_vtu );
+                kvs::UnstructuredVolumeObject* object = &importer;
+
+                auto cell_type = object->cellType();
+                sub_volume_counts[cell_type] = ( sub_volume_counts.count( cell_type ) == 0 )
+                                                    ? 1
+                                                    : ( sub_volume_counts[cell_type] + 1 );
+                veclens[cell_type] = object->veclen();
+            }
+        }
+    }
+
+    for ( auto& e : sub_volume_counts )
+    {
+        auto cell_type = e.first;
+
+        pfi_map.emplace( static_cast<int>( cell_type ),
+                            cvt::UnstructuredPfi( veclens[cell_type], last_time_step,
+                                                sub_volume_counts[cell_type] ) );
+    }
+
+    for ( auto& e : sub_volume_counts )
+    {
+        sub_volume_ids[e.first] = 1;
+    }
+
+    for ( auto format : input_vtm.eachBlock() )
+    {
+        if ( auto input_vtu = dynamic_cast<cvt::VtkXmlUnstructuredGrid*>( format.get() ) )
+        {
+            for ( auto vtu : input_vtu->eachCellType() )
+            {
+                cvt::VtkImporter<cvt::VtkXmlUnstructuredGrid> importer( &vtu );
+                kvs::UnstructuredVolumeObject* object = &importer;
+                object->print( std::cout, kvs::Indent( 2 ) );
+
+                std::cout << "  Writing to " << directory << " ..." << std::endl;
+                auto local_base =
+                    std::string( base ) + "_" + std::to_string( object->cellType() );
+
+                cvt::UnstructuredVolumeObjectExporter exporter( &importer );
+                exporter.setWritingDataTypeToExternalBinary();
+                exporter.write( directory, local_base, time_step,
+                                sub_volume_ids[object->cellType()],
+                                sub_volume_counts[object->cellType()] );
+
+                pfi_map.at( static_cast<int>( object->cellType() ) )
+                    .registerObject( &exporter, time_step, sub_volume_ids[object->cellType()] );
+
+                ++sub_volume_ids[object->cellType()];
+            }
+        }
+        else
+        {
+            std::cout << "An unsupported vtk data type" << std::endl;
+        }
+    }
+
+    cvt::Pfl pfl;
+    for ( auto& e : pfi_map )
+    {
+        std::string local_base = std::string( base ) + "_" + std::to_string( e.first );
+        e.second.write( directory, local_base );
+        e.second.print( std::cout );
+
+        pfl.registerPfi( directory, local_base );
+    }
+    pfl.write( directory, base );
+}
+
 void SeriesVtm2Kvsml( const std::string& directory, const std::string& base,
                       const std::string& src )
 {
