77dc1a18346af7b664614bce867e6742b6db57cd
sakamoto-naohito <sakamoto.naohito@jaea.go.jp>
2024-12-02 16:47:57 +0900

    ディレクトリClient,gluiを削除

--- CS_server/Client/sighandler.h
+++
@@ -1,9 +0,0 @@
-#ifndef PBVR__SIG_HANDLER_H_INCLUDE
-#define PBVR__SIG_HANDLER_H_INCLUDE
-
-void InitializeSignalHandler();
-void SignalHandler( const int sig );
-bool IsSignal();
-
-#endif    // PBVR__SIG_HANDLER_H_INCLUDE
-
