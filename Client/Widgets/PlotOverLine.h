4f17aeaf894d2de4226f44a8fc5950384f591d42
TO0603 <to0603@t0603MacBook.edo.jaea.go.jp>
2024-12-25 13:35:40 +0900

    グラフウィジェットを追加

--- Client/Widgets/PlotOverLine.h
+++ Client/Widgets/PlotOverLine.h
@@ -2,7 +2,7 @@
 #define PLOTOVERLINE_H
 
 #include <QDockWidget>
-
+#include <kvs/ValueArray>
 class Connect;
 
 namespace Ui {
@@ -16,12 +16,25 @@ class PlotOverLine : public QDockWidget
 public:
     explicit PlotOverLine(QWidget *parent = nullptr, Connect* Connect = nullptr);
     ~PlotOverLine();
+    void setPlotData( kvs::ValueArray<float> xAxis, kvs::ValueArray<bool> mask, kvs::ValueArray<float> values );
 
 private:
     Ui::PlotOverLine *ui;
     Connect* m_connect; //クライアント/サーバメッセージ呼び出し用。
 
+    //TESTDATA
+    kvs::ValueArray<float> xAxis;
+    kvs::ValueArray<bool>  mask;
+    kvs::ValueArray<float> values;
+    void initData();
+
+    double m_x_min;
+    double m_x_max;
+    double m_y_min;
+    double m_y_max;
+
 private slots:
+    void onResetButtonClicked();
     void onResolutionValueChanged(); //Resolutionのスピンボックスの値が変更された際に呼び出されるスロット
     void onStartPointAxisValueChanged(); //StartのXYZのダブルスピンボックスの値が変更された際に呼び出されるスロット
     void onEndPointAxisValueChanged(); //EndのXYZのダブルスピンボックスの値が変更された際に呼び出されるスロット
