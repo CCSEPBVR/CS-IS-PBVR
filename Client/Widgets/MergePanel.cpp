32f449a87f50d91211b2a6523e03abb8cfc80fe8
TO0603 <osaki.tsukasa@jaea.go.jp>
2025-03-24 11:40:20 +0900

    MergeパネルApply時にクラッシュする不具合を修正

--- Client/Widgets/MergePanel.cpp
+++ Client/Widgets/MergePanel.cpp
@@ -1261,14 +1261,17 @@ void MergePanel::totalParticles()
             m_files_manager[row]->getFormat() == FilesManager::PointObjectLAS ||
             m_files_manager[row]->getFormat() == FilesManager::PointObjectPTS )
         {
-            if( m_pbvr_gui->screen()->scene()->object( m_files_manager[row]->getIDs().first ) != nullptr )
+            if( m_files_manager[row]->getIDs().first != -1 && m_files_manager[row]->getIDs().second != -1 )
             {
-                auto* object = m_pbvr_gui->screen()->scene()->object( m_files_manager[row]->getIDs().first );
-                if( object && object->isVisible() )
+                if( m_pbvr_gui->screen()->scene()->object( m_files_manager[row]->getIDs().first ) != nullptr )
                 {
-                    if (auto* pointObject = dynamic_cast<kvs::PointObject*>(object))
+                    auto* object = m_pbvr_gui->screen()->scene()->object( m_files_manager[row]->getIDs().first );
+                    if( object && object->isVisible() )
                     {
-                        totalParticles += pointObject->numberOfVertices();
+                        if (auto* pointObject = dynamic_cast<kvs::PointObject*>(object))
+                        {
+                            totalParticles += pointObject->numberOfVertices();
+                        }
                     }
                 }
             }
