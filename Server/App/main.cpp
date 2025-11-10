ebb12222cbbeb313511b6fe971c972cfed4669a1
TO0603 <osaki.tsukasa@jaea.go.jp>
2025-11-10 10:04:17 +0900

    style: main.cppのコード整形とデフォルトポート番号コメントを追加

--- Server/App/main.cpp
+++ Server/App/main.cpp
@@ -1,11 +1,11 @@
 #include "Server.h"
-int main(int argc, char *argv[])
+int main( int argc, char *argv[] )
 {
-    int port = 60000;
+    int port = 60000; // デフォルトポート番号
 
     if( argc >= 2 )
     {
-        port = std::atoi(argv[1]);
+        port = std::atoi( argv[1] );
     }
 
     Server server( port );
