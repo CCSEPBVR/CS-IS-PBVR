233ade83363eed0b68bfef399ff437d75fca4a7d
sakamoto-naohito <sakamoto.naohito@jaea.go.jp>
2026-02-18 16:23:22 +0900

    IS Hydrogen OpenFOAM 実行環境の整備 - Makefileの修正 - パラメータファイルの修正 - PlotOverTimeのパラメータファイルの新規作成 - RESET.shの修正 - Hydrogenのmain.cppの修正

--- Server/Example/C/s86_mpi_omp/Hydrogen_unstruct/RESET.sh
+++ Server/Example/C/s86_mpi_omp/Hydrogen_unstruct/RESET.sh
@@ -6,3 +6,4 @@ rm state.txt
 cp default_old.tf default.tf
 cp parameter_old.gly parameter.gly
 cp parameter_old.pol parameter.pol
+cp parameter_old.pot parameter.pot
--- Server/Example/C/s86_mpi_omp/Hydrogen_unstruct_prism/main.cpp
+++ Server/Example/C/s86_mpi_omp/Hydrogen_unstruct_prism/main.cpp
@@ -1,9 +1,12 @@
+#include "Hydrogen.h"
 #include <string>
 #include <iostream>
 #include <cstdlib>
-#include "Hydrogen.h"
 #include "kvs_wrapper.h"
+#include "kvs_wrapper_common.h"
+#ifndef CPU_VER
 #include <mpi.h>
+#endif
 
 #define RANK 1
 
@@ -16,15 +19,19 @@
 /*===========================================================================*/
 int main( int argc, char** argv )
 {
-     MPI_Init( &argc, &argv );
     int mpi_rank;
+#ifndef CPU_VER
+    MPI_Init( &argc, &argv );
     MPI_Comm_rank( MPI_COMM_WORLD, &(mpi_rank) );
+#else
+    mpi_rank = 0;
+#endif
 
     // Generate hydrogen volume.
     Hydrogen hydro;
 
     //全体の座標
-    domain_parameters dom = {
+    domain_parameters_unstruct dom = {
         hydro.global_min_coord.x(),
         hydro.global_min_coord.y(),
         hydro.global_min_coord.z(),
@@ -34,22 +41,25 @@ int main( int argc, char** argv )
     };
 
     int time_step = 0;
+    bool result = false;
 
     begin_wrapper_async_io();
     for(;;)
     {
         if(mpi_rank==0) hydro.show();
 
-        generate_particles( time_step, dom,
+        result = generate_particles( time_step, dom,
                             hydro.values, hydro.nvariables,
                             hydro.coords, hydro.nnodes,
-                            hydro.connections, hydro.ncells, pbvr::VolumeObjectBase::CellType::Prism );
-
+                            hydro.connections, hydro.ncells, vismodule::VolumeObjectBase::CellType::Prism );
+        if ( !result ) return -1;
         time_step++;
     }
     end_wrapper_async_io();
 
+#ifndef CPU_VER
     MPI_Finalize();
+#endif
 
     return 0;
 }
