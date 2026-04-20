a6bb3a0292fcbabdd8331ae8ca4f6a40e2e812cb
sakamoto-naohito <sakamoto.naohito@jaea.go.jp>
2026-04-20 10:58:17 +0900

    環境変数KVS_DIRなしでShaderディレクトリを参照するように修正

--- Client/App/main.cpp
+++ Client/App/main.cpp
@@ -1,10 +1,16 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <QCoreApplication>
+#include <QDir>
+#include <kvs/ShaderSource>
 
 int main(int argc, char *argv[])
 {
     kvs::qt::Application app( argc, argv );
+
+    const QString shaderDir = QDir( QCoreApplication::applicationDirPath() ).filePath( "Shader" );
+    kvs::ShaderSource::AddSearchPath( shaderDir.toStdString() );
 
     MainWindow mainWindow( app );
     mainWindow.adjustSize();
