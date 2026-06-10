4fd09412b555d8abf1d11064a65b839205e566b5
下村和也 <shimomura.kazuya@jaea.go.jp>
2026-06-10 14:18:30 +0900

    ensemble TFE について、初期状態の統計量選択を平均に変更
     -初回同通時に平均を選択状態にするように変更

--- Client/App/MainWindow.cpp
+++ Client/App/MainWindow.cpp
@@ -614,6 +614,7 @@ void MainWindow::initializeEnsembleTransferFunctionEditor()
         m_ensemble_transfer_function_editor->adjustSize();
         m_ensemble_transfer_function_editor->close();
         connect( m_ensemble_transfer_function_editor, &EnsembleTransferFunctionEditor::statisticChanged, m_object_editor, &ObjectEditor::setRequestedStatistic );
+        m_object_editor->setRequestedStatistic( QStringLiteral( "average" ) );
     }
 }
 
